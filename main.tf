4e626b2deadee7376178c361fff369c52c4e2a77
Umesh-Phendarkar <umesh.phendarkar@gmail.com>
2025-03-17 10:41:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,17 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.0"
-    }
+      source = "hashicorp/azurerm"
+      version = ">= 3.59.0"
+    } 
   }
+  required_version = ">= 0.14.9"
+}
 
 provider "azurerm" {
   features {}
-}
+
+  skip_provider_registration = "true"
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resource-group"
